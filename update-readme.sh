14c929ecd5814ad0e460d388d00a96bcbb8ca243
Tripathi <tri.utkarsh7596@gmail.com>
2020-07-19 17:48:08 +0530

    update script

--- update-readme.sh
+++ update-readme.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-echo Table Of Content > README.md
+echo \# Table Of Content > README.md
 echo >> README.md
 
-ls | grep [0-9].*.md | sed "s|\(.*\)|[\1](\1)|" >> README.md
+ls | grep [0-9].*.md | sed "s|\(.*\)|[\1](\1)\n|" >> README.md
 
 
